acbfe40035c919ce570d5d27e9d3f513ce39b495
muzakonthecode <mahmadzekoo@gmail.com>
2022-10-08 07:37:49 +0200

    1

---
+++ engine.py
@@ -0,0 +1,10 @@
+""" 
+This class is responsible for storing all the information about the current state of a chess game. it will also be responsible for determining the valid moves in the current state. and it will also keep a move log.
+
+"""
+
+class GameState():
+    def __init__(self):
+        self.board = [
+            []
+        ]
