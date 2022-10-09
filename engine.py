fcba3776433fc74ff7fe42714340097ffc3237d4
muzakonthecode <mahmadzekoo@gmail.com>
2022-10-09 14:42:10 +0200

    3

--- engine.py
+++ engine.py
@@ -6,6 +6,19 @@ This class is responsible for storing all the information about the current stat
 
 class GameState():
     def __init__(self):
+        # board is an 8x8 2d list, each element of the list has 2 characters.
+        # the first character represents the color of the piece, 'b' or 'w'
+        # the second character represents the type of the piece, 'K','Q','R','B','N' or 'p'
+        # "--" represents an empty space with no piece
         self.board = [
-            []
+            ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
+            ["bp", "bp", "bp", "bp", "bp", "bp", "bp", "bp"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"],
+            ["wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp"]
         ]
+        self.whiteToMove = True
+        self.moveLog = []
--- main.py
+++ main.py
@@ -2,3 +2,50 @@
 This is the main driver file. it will be responsible for handling user input and displaying the current GameState Object.
 
 """
+import pygame as p
+from chess import engine
+
+WIDTH = HEIGHT = 512  # 400 is another good option
+DIMENSION = 8  # dimensions of a chess board are 8x8
+SQ_SIZE = HEIGHT // DIMENSION
+MAX_FPS = 15  # for animations later on
+IMAGES = {}
+
+'''
+Initialize a global directory of images. this will be called exactly once in the main
+'''
+
+
+def loadImages():
+    pieces = ['wp', 'wR', 'wN', 'wB', 'wK',
+              'wQ', 'bp', 'bR', 'bN', 'bB', 'bK', 'bQ']
+    for piece in pieces:
+        IMAGES[piece] = p.transform.scale(p.image.load(
+            "images/" + piece + ".png"), (SQ_SIZE, SQ_SIZE))
+    # Note: we can access an image by saying for example 'IMAGES['wp']'
+
+
+'''
+the main driver for our code. this will handle user input and updating the graphics 
+'''
+
+
+def main():
+    p.init()
+    screen = p.display.set_mode((WIDTH, HEIGHT))
+    clock = p.time.Clock()
+    screen.fill(p.Color("white"))
+    gs = engine.GameState()
+    loadImages()  # only do this once, before the while loop
+    running = True
+    while running:
+        for e in p.event.get():
+            if e.type == p.QUIT:
+                running = False
+
+        clock.tick(MAX_FPS)
+        p.display.flip
+
+
+if __name__ == "__main__":
+    main()
